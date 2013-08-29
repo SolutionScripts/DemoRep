369c94e86c6a8e76a9f5657629ae32ad7d285a98
Ashish Patel <patelashish_90@yahoo.com>
2013-08-30 00:43:47 +0530

    contact chaanges
    yt

--- Website/Contact.aspx.cs
+++ Website/Contact.aspx.cs
@@ -13,7 +13,7 @@ public partial class Contact : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+//retttttttttttttttttttttttttttttttttt
     }
     protected void SubmitButton_Click(object sender, EventArgs e)
     {
